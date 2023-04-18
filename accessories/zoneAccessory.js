997fdccd9f2420ae89cf20c5a0df95184c9ef2da
haywirecoder <44388297+haywirecoder@users.noreply.github.com>
2023-04-17 21:54:18 -0400

    Add files via upload

--- accessories/zoneAccessory.js
+++ accessories/zoneAccessory.js
@@ -1,9 +1,10 @@
 "use strict";
+var tpidefs = require('./../tpi.js');
 
 const ENVISALINK_MANUFACTURER = "Envisacor Technologies Inc."
 
 class EnvisalinkZoneAccessory {
-  constructor(log, config, Service, Characteristic, UUIDGen) {
+  constructor(log, config, Service, Characteristic, UUIDGen, alarm) {
     this.Characteristic = Characteristic;
     this.Service = Service;
     this.log = log;
@@ -13,6 +14,8 @@ class EnvisalinkZoneAccessory {
     this.accessoryType =  "sensor";
     this.zoneNumber = config.zoneNumber;
     this.pin = config.pin;
+    this.alarm = alarm;
+    this.bypassStatus = false;
     this.uuid = UUIDGen.generate(this.config.serialNumber);
     
 
@@ -59,7 +62,21 @@ class EnvisalinkZoneAccessory {
           motionService.setCharacteristic(this.Characteristic.StatusTampered, this.Characteristic.StatusTampered.NOT_TAMPERED);
           this.service = motionService;
           this.bypassEnabled = this.config.bypassEnabled ? this.config.bypassEnabled : false;
+          this.commandTimeOut = this.config.commandTimeOut;
           this.envisakitCurrentStatus = "close";
+
+          // Bypass switch for individual sensor only if the master bypass switch is disabled. 
+          var swbypass = this.accessory.getService(this.Service.Switch);
+          if(this.bypassEnabled && !this.config.masterBypass) {
+              if(swbypass == undefined) swbypass = this.accessory.addService(this.Service.Switch,this.name + " bypass"); 
+              swbypass.getCharacteristic(this.Characteristic.On) 
+                  .on('get', async callback => this.getByPass(callback))
+                  .on('set', async (state, callback) => this.setByPass(state, callback));  
+          }
+          else {
+            if (swbypass!= undefined) this.accessory.removeService(swbypass);
+          }
+
       break;
   
       case "door":
@@ -73,7 +90,21 @@ class EnvisalinkZoneAccessory {
           contactService.setCharacteristic(this.Characteristic.StatusTampered, this.Characteristic.StatusTampered.NOT_TAMPERED);
           this.service = contactService;
           this.bypassEnabled = this.config.bypassEnabled ? this.config.bypassEnabled : false;
+          this.commandTimeOut = this.config.commandTimeOut;
           this.envisakitCurrentStatus = "close";
+
+          // Bypass switch for individual sensor only if the master bypass switch is disabled. 
+          var swbypass = this.accessory.getService(this.Service.Switch);
+          if(this.bypassEnabled && !this.config.masterBypass) {
+              if(swbypass == undefined) swbypass = this.accessory.addService(this.Service.Switch,this.name + " bypass"); 
+              swbypass.getCharacteristic(this.Characteristic.On) 
+                  .on('get', async callback => this.getByPass(callback))
+                  .on('set', async (state, callback) => this.setByPass(state, callback));  
+          }
+          else {
+            if (swbypass!= undefined) this.accessory.removeService(swbypass);
+          }
+         
       break;
   
       case "leak":
@@ -87,6 +118,19 @@ class EnvisalinkZoneAccessory {
           this.service = leakService;
           this.bypassEnabled = this.config.bypassEnabled ? this.config.bypassEnabled : false;
           this.envisakitCurrentStatus = "close";
+
+          // Bypass switch for individual sensor only if the master bypass switch is disabled. 
+          var swbypass = this.accessory.getService(this.Service.Switch);
+          if(this.bypassEnabled && !this.config.masterBypass) {
+              if(swbypass == undefined) swbypass = this.accessory.addService(this.Service.Switch,this.name + " bypass"); 
+              swbypass.getCharacteristic(this.Characteristic.On) 
+                  .on('get', async callback => this.getByPass(callback))
+                  .on('set', async (state, callback) => this.setByPass(state, callback));  
+          }
+          else {
+            if (swbypass!= undefined) this.accessory.removeService(swbypass);
+          }
+
       break;
   
       case "smoke":
@@ -100,6 +144,7 @@ class EnvisalinkZoneAccessory {
           this.service = SmokeSensorService;
           this.bypassEnabled = this.config.bypassEnabled ? this.config.bypassEnabled : false;
           this.envisakitCurrentStatus = "close";
+
       break;
   
       case "co":
@@ -113,7 +158,7 @@ class EnvisalinkZoneAccessory {
            this.service = CarbonMonoxideSensorService;
            this.bypassEnabled = this.config.bypassEnabled ? this.config.bypassEnabled : false;
            this.envisakitCurrentStatus = "close";
-
+           
       break;
     }
   }
@@ -138,6 +183,68 @@ class EnvisalinkZoneAccessory {
     callback(null, this.ENVISA_TO_HOMEKIT_CO[this.envisakitCurrentStatus]);
   }
 
+  async getByPass(callback) {
+    
+    return callback(null,this.bypassStatus);
+    
+  }
+
+  // Timer triggered event if bypass does not occur in an allocated time frame.
+  processBypassTimer() {
+    if (this.alarm.isProcessingBypass) {
+        this.log.warn(`Bypass request did not return successfully in the allocated time.`);
+        this.alarm.isProcessingBypass = false;
+        this.alarm.isProcessingBypassqueue = 0;
+    } 
+  }
+
+async setByPass(value, callback) {
+    this.log.debug("setByPass: zone - ", this.name + ", " + this.zoneNumber); 
+
+    if (this.alarm.isProcessingBypass) {
+      this.log(`Already processing Bypass request. Command ignored.`);
+      return callback(null);
+    }
+    // Bypass is only available if system is not armed, alarm or in-trouble state.
+    if (!this.alarm.alarmSystemMode.includes('ALARM') && !this.alarm.alarmSystemMode.includes('ARMED')) {
+
+      var l_zonesToBypass;
+      var l_alarmCommand;
+      this.alarm.isProcessingBypass = true;
+
+      // If switch is bypass is on for zone, clear system (disarm system), otherwise bypass selected zone.
+      if (value)
+      {
+        this.log(`Requesting bypassing of ${this.name} ...`)
+        // Require leading zero for zone numbers which are not two or three digit (128 Panel)
+        if (this.deviceType == "128FBP") 
+          l_zonesToBypass = (("00" + this.zoneNumber).slice(-3));
+        else
+          l_zonesToBypass  = (("0" + this.zoneNumber).slice(-2));
+      
+        l_alarmCommand = this.pin + tpidefs.alarmcommand.bypass + l_zonesToBypass;
+        this.alarm.isProcessingBypassqueue = 1;
+    
+      }
+      else {
+        this.log(`Removing bypassing of ${this.name} ...`);
+      l_alarmCommand = this.pin + tpidefs.alarmcommand.disarm;
+      } 
+    
+      // Set busy status on process Bypass
+      this.byPassTimeOut = setTimeout(this.processBypassTimer.bind(this), this.commandTimeOut * 1000);
+      this.alarm.sendCommand(l_alarmCommand);
+      await new Promise(r => setTimeout(r, 2000));
+      // Set current of switch
+      this.bypassStatus = value;
+    }
+    else
+    {
+      this.bypassStatus = !value;
+      this.log(`Alarm is ${this.alarm.alarmSystemMode} no action required. Ignoring bypass request.`);
+    } 
+    return callback(null)
+  }
 }
 
 module.exports = EnvisalinkZoneAccessory;
